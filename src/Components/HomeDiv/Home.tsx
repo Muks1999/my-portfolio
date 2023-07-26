0fb8c0771c01955593494a827ddd2cb60772c6b5
MukulInnobit <mukul.agrawal@innobitsystems.com>
2023-07-26 21:07:47 +0530

    Decrease white background

--- src/Components/HomeDiv/Home.tsx
+++ src/Components/HomeDiv/Home.tsx
@@ -15,7 +15,7 @@ export const Home = () => {
         src="https://images.unsplash.com/photo-1510915228340-29c85a43dcfe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
         alt=""
       />
-      <div className="w-full h-screen absolute top-0 left-0 bg-white/40">
+      <div className="w-full h-screen absolute top-0 left-0 bg-white/30">
         <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center">
           <h1 className="sm:text-5xl text-4xl font-bold text-gray-800">
             I'm Mukul Agrawal
